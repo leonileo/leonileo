706f36159431e65cb2919e74082e719836a1ad7d
leonileo <leolittleprogrammer@gmail.com>
2025-01-08 21:45:43 +0300

    hot fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
         </div>
         <div className="socials">
           <p className="font-bold mb-1">Socials</p>
-          <a className="block" href="https://https://github.com/leonileo" target="_blank" >Github</a>
+          <a className="block" href="https://github.com/leonileo" target="_blank" >Github</a>
           <a className="block" href="https://www.linkedin.com/in/kaleb-wendwessen/" target="_blank" >Linkedin</a>
           <a className="block" href="https://x.com/kalebleonileo" target="_blank" >x (twitter)</a>
           <a className="block" href="https://instagram.com/kalebleo4" target="_blank" >Instagram</a>
